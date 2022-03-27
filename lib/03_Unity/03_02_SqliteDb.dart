578b42955d7401cc63383b911f540d24adbd923d
RikitoKarita <rktkrt12@gmail.com>
2022-03-27 10:21:29 +0900

    GoogleMapのpkg install

--- lib/03_Unity/03_02_SqliteDb.dart
+++ lib/03_Unity/03_02_SqliteDb.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:flutter/cupertino.dart';
-
 import '03_01_SharedPreferences.dart';
 
 class QuizStatusDb {
